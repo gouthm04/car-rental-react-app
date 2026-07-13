45d81a4707f29e9961ec0185dfbd6455de772f72
gouthm.04 <gouthm.04@gmail.com>
2026-07-13 21:46:12 +0530

    Add Home  page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import Home from './components/Home'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <Home />
     </>
   )
 }
