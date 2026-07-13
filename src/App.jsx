3df08d08622e7ba0d06cdd5964861bacc40ffbcf
gouthm.04 <gouthm.04@gmail.com>
2026-07-13 22:46:29 +0530

    add view cars

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Home from './components/Home'
 import './App.css'
 import AddCar from './components/AddCar'
+import ViewCar from './components/ViewCar'
 
 function App() {
   return (
     <>
-      <AddCar />
+      <ViewCar   />
     </>
   )
 }
